0a1f106d474d1eb373cc7e408cd7ef2cacee6eb9
Chris Lynch <chrislynch42@yahoo.com>
2015-05-04 13:55:26 -0400

    learning functional programming in java

--- certification/functional/venkat-book/src/main/java/com/codeexcursion/functional/Chapter1.java
+++ certification/functional/venkat-book/src/main/java/com/codeexcursion/functional/Chapter1.java
@@ -1,6 +1,9 @@
 package com.codeexcursion.functional;
 
 import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * Hello world!
@@ -11,26 +14,33 @@ public class Chapter1 {
   private final List<String> friends = Arrays.asList("Brian", "Nate", "Neal", "Raju", "Sara", "Scott");
 
   public void friends() {
+    final Consumer<String> printer
+            = (String name) -> {
+              System.out.println(name);
+              System.out.println(Thread.activeCount());
+            };
+
     StopWatch stopWatch = new StopWatch("Old Friends");
     for (int i = 0; i < friends.size(); i++) {
       System.out.println(friends.get(i));
+      System.out.println(Thread.activeCount());
     }
     stopWatch.stop();
 
     stopWatch = new StopWatch("Recent Friends");
     for (String name : friends) {
       System.out.println(name);
+      System.out.println(Thread.activeCount());
     }
     stopWatch.stop();
 
     stopWatch = new StopWatch("New Friends");
-    friends.forEach(name -> System.out.println(name));
+    friends.forEach(printer);
     stopWatch.stop();
-    
+
     stopWatch = new StopWatch("Many Friends");
-    friends.parallelStream().forEach(name -> System.out.println(name));
+    friends.parallelStream().forEach(printer);
     stopWatch.stop();
-    
 
   }
 
